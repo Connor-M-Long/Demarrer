e1f733a45302ac3706e5fa0e7bc929499d6ec9d2
JapaneseNoodles <117811551+JapaneseNoodles@users.noreply.github.com>
2025-06-14 15:16:31 +0200

    New delete functions, new validation, logger has been implemented

--- backend/main.py
+++ backend/main.py
@@ -48,6 +48,10 @@ db_dependency = Annotated[Session, Depends(get_db)]
 memory_db_startup = {"startup": []}
 memory_db_jobroles = {"jobroles": []}
 
+async def Logger(user: str, action: str, GUID: str):
+    with open("logs.txt", "a") as f:
+        f.write(f"{GUID} | {user} has {action}\n")
+
 @app.get("/startups", response_model=None)
 async def get_StartUps(db: db_dependency):
 
@@ -69,12 +73,16 @@ async def add_StartUp(request: StartUp, db: db_dependency):
         Stage=request.Stage,
         Founder=request.Founder,
         Field=request.Field)
-
-    db.add(db_post)
-    db.commit()
-    db.refresh(db_post)
-
-    return "Success"
+    
+    if db_post.Name == "" or db_post.Description == "" or db_post.Stage == "" or db_post.Founder == "" or db_post.Field == "":
+        return "Error, please fill in all fields"
+    else:
+        db.add(db_post)
+        db.commit()
+        db.refresh(db_post)
+        await Logger("Connor", "created a startup", "7171f1b1-ea0a-44c1-a134-f3152cb4aeb5")
+        return "Success"
+    
 
 @app.get("/jobroles/{companyname}", response_model=None)
 async def get_JobRoles(companyname: str, db: db_dependency):
@@ -97,10 +105,29 @@ async def add_JobRole(request: JobRole, db: db_dependency):
         StartDate = request.StartDate
     )
 
-    db.add(db_post)
-    db.commit()
-    db.refresh(db_post)
+    if db_post.CompanyName == "" or db_post.JobTitle == "" or db_post.JobDescription == "" or db_post.StartDate == "":
+        return "Error, please fill in all fields"
+    else:
+        db.add(db_post)
+        db.commit()
+        db.refresh(db_post)
+        await Logger("Connor", "created a job role", "ce32a32a-61dc-458a-a443-5ff00fd6cc72")
+        return "Success"
 
+@app.delete("/deleteStartUp/{id}", response_model=None)
+async def delete_StartUp(id: int, db: db_dependency):
+
+    db.query(dbSchema.startUpModel).filter(dbSchema.startUpModel.id == id).delete()
+    db.commit()
+    await Logger("Connor", "deleted a startup", "df748e6f-2e18-4e9a-8b76-2a56ffe09e5b")
+    return "Success"
+
+@app.delete("/deleteJobRole/{id}", response_model=None)
+async def delete_JobRole(id: int, db: db_dependency):
+
+    db.query(dbSchema.JobRole).filter(dbSchema.JobRole.id == id).delete()
+    db.commit()
+    await Logger("Connor", "deleted a job role", "e962345a-f61e-4cef-b663-280aaec107a6")
     return "Success"
 
 if __name__ == "__main__":
--- frontend/src/pages/HomePage/HomePage.tsx
+++ frontend/src/pages/HomePage/HomePage.tsx
@@ -5,8 +5,19 @@ function Home() {
     return (
         <>
 
-            yo
-
+            <div className="bg-red-500 w-full">
+                <div className="">
+                <h1 className="text-3xl font-bold underline md:flex">
+                    Demarrer
+                </h1>
+                </div>
+                <div className="">
+                <p>
+                    The greatest stories start in humble beginnings.
+                </p>
+                </div>
+            </div>
+            
         </>
     )
 }
