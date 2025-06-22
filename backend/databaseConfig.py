10fa4015f5fb8a96bf712c7a971da7b9a39aa72e
Connor Long <117811551+Connor-M-Long@users.noreply.github.com>
2025-06-22 10:58:40 +0200

    Update databaseConfig.py

--- backend/databaseConfig.py
+++ backend/databaseConfig.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:Metallica@localhost:5432/Demarrer"
+DATABASE_URL = "postgresql://postgres:[PASSWORD]@localhost:5432/Demarrer"
 
 engine = create_engine(DATABASE_URL)
 
